28252a305966c4dd12b488d594c8f65d40f2e0db
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-06-10 13:41:26 -0300

    Refactor server.js to serve the homepage and improve static file handling

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ const reservasRouter = require('./routes/reservas')
 
 const app = express()
 const port = process.env.PORT|| 3000;
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(reservasRouter);
 
 app.get('/', (req, res) => {
-    res.send('API de reservas online!')
-})
+    res.sendFile(path.join(__dirname, '../public/html/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`API de reservas rodando na porta ${port}`)
